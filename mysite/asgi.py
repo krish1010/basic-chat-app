ddf2e20e1719aa961a9bb6b866b62ee4e07170c0
krish1010 <krish.krishanu10@gmail.com>
2020-11-23 12:24:36 +0530

    removed assignment of get_asgi_application beforehand

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -3,12 +3,10 @@ import os
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
